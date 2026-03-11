9c5b376e571d08ab3cba73f54a7206cd69119787
Karina Valdés Iglesias <gcvaldesiglesiaskarina@gmail.com>
2026-03-11 13:10:03 -0600

    Add:test

--- tests/Calculadora.Tests/OperacionesMatematicas.Test.cs
+++ tests/Calculadora.Tests/OperacionesMatematicas.Test.cs
@@ -1,9 +1,58 @@
-using Calculadora
+using Calculadora;
 
 namespace Calculadora.Tests;
 
 public class OperacionesMatematicasTest
 {
-    
-    
+    [Fact]
+
+    public void Sumar_DosPositivos_RetornaSuma()
+    {
+        // Arrange
+        var operacionesMatematicas = new OperacionesMatematicas();
+
+        int a= 4;
+        int b= 5;
+
+        // Act
+        var resultado = operacionesMatematicas.Sumar(a, b);
+
+        // Assert
+        Assert.Equal(9, resultado);
+    }
+
+    [Fact]
+
+    public void Sumar_NegativoPositivo_RetornaValorCorrecto()
+    {
+        // Arrange
+        var operacionesMatematicas = new OperacionesMatematicas();
+
+        int a = -4;
+        int b = 5;
+
+        // Act
+        var resultado = operacionesMatematicas.Sumar(a, b);
+
+        // Assert
+        Assert.Equal(1, resultado);
+    }
+
+    [Fact]
+    public void Sumar_CeroMasCero_RetornaCero()
+    {
+        // Arrange
+        var operacionesMatematicas = new OperacionesMatematicas();
+
+        int a = 0;
+        int b = 0;
+
+        // Act
+        var resultado = operacionesMatematicas.Sumar(a, b);
+
+        // Assert
+        Assert.Equal(0, resultado);
+    }
+
+
 }
